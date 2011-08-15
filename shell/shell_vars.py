ac3636ee4ee07cb6a0aad9023d37b189e12bc0ae
= <pete@daffodil.uk.com>
2011-08-15 22:46:54 +0100

    live test

--- shell/shell_vars.py
+++ shell/shell_vars.py
@@ -5,7 +5,7 @@ import memcache
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 # Max number of servers to look
-MAX_MPSERVER_ADDRESS = 8 #20
+MAX_MPSERVER_ADDRESS = 20
 
 MC = memcache.Client(['127.0.0.1:11211'])
 

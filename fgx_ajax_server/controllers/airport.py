7f02e4034a7664324959a5f74090378455c79626
= <pete@daffodil.uk.com>
2011-08-22 14:29:04 +0100

    runways

--- fgx_ajax_server/controllers/airport.py
+++ fgx_ajax_server/controllers/airport.py
@@ -50,70 +50,62 @@ class AirportController(BaseController):
 			payload['app_error'] = "Airport '%s' not found" % code
 			
 		else:
-			data = {'airport': [airport.dic()]}
+			airport_dic = {'airport': [airport.dic()]}
 			runwayObs = Session.query(RunwayThreshold).filter_by(apt_icao=code).order_by(RunwayThreshold.rwy_num).all()
 			ilsObs = Session.query(Ils).filter_by(apt_icao=code).order_by(Ils.rwy_num).all()
 			
-			## walk thru runways and stick in a dit, along with ils
+			## walk thru runways and stick in a dic, along with ils
 			runways_dic = {}
 			runway_ends = []
 			for runway in runwayObs:
-				rwy_num = runway.rwy_num.strip()
-				runways_dic[rwy_num] = runway.dic()
-				runways_dic[rwy_num]['ils'] = None
-				for ils in ilsObs:
-					if rwy_num == ils.rwy_num:
-						runways_dic[rwy_num] = ils.dic()
-				rev_rwy = self.reverse_rwy(runway.rwy_num)
-				print rwy_num, rev_rwy
-				if not rev_rwy in runways_dic:
-					runway_ends.append( (rwy_num, rev_rwy) )
-			print runway_ends
 				
-				
-			if len(runwayObs) > 0:
-				runways = []
-				for runway in runwayObs:
-					dic = runway.dic()
-					print runway.rwy_num, self.reverse_rwy(runway.rwy_num)
-					dic['ils'] = None
+				rwy_num = str(runway.rwy_num.strip())
+				if not rwy_num in runways_dic: ## WTF dupes ??
+					runways_dic[rwy_num] = runway.dic()
+					runways_dic[rwy_num]['ils'] = None
 					for ils in ilsObs:
-						if ils.rwy_num == ils.rwy_num:
-							dic['ils'] = ils.dic()
-					runways.append(dic)
-				data['runways'] = runways
-			else:
-				data['runways'] = []
+						if rwy_num == ils.rwy_num:
+							runways_dic[rwy_num] = ils.dic()
+					rev_rwy = self.reverse_rwy(runway.rwy_num)
+					
+					if not runways_dic.has_key(rev_rwy):
+						runway_ends.append( [rwy_num, rev_rwy] )
+	
+
+			
+			airport_dic['runways'] = []
+			for ends in runway_ends:
+				dic = {}
+				dic['runway'] = "%s-%s" % (ends[0], ends[1])
+				dic['thresholds'] =  [ runways_dic[ends[0]], runways_dic[ends[1]] ]
+				airport_dic['runways'].append( dic )
 		
-			payload['airport'] = data
+			payload['airport'] = airport_dic
 		return payload
 		
 		
 		
 	def reverse_rwy(self, rwy_num):
-		rwy_str = rwy_num
-		parts = re.compile("(\d+)(.*)").split(rwy_str, maxsplit=0)
-		print rwy_str, len(rwy_str), parts
+		parts = re.compile("(\d+)(.*)").split(rwy_num, maxsplit=0)
+		#print rwy_str, len(rwy_str), parts
 		num = int(parts[1])
-		lr = parts[2]
-		
-		#if(!$num)
-		#{
-		#	return ${rwy_num};
-		#}
+		lr = parts[2].upper() 
+
+		#if num:
+		#	return rwy_str	
 		
 		if num > 18:
-			num = num - 18
+			rnum = num - 18
 		else:
-			num += num + 18
-
-		if lr != "":		
-			if lr.upper() == "L":
+			rnum = num + 18
+			
+		if lr != "":
+			if lr == "L":
 				lr = "R"
-			else:
+			elif lr == "R":
 				lr = "L"
-		
-		return "%s%s" % (num, lr)
+
+		return str("%02d%s" % (rnum, lr)).strip()
 
 
 """
--- fgx_ajax_server/model/__init__.py
+++ fgx_ajax_server/model/__init__.py
@@ -29,7 +29,6 @@ t_apt =  Table('apt', metadata,
 )
 
 class Apt(object):
-	
 	def dic(self):
 		return {'ogc_fid': self.ogc_fid,
 				'apt_icao': self.apt_icao.strip(),
@@ -57,7 +56,6 @@ t_ils =  Table('ils', metadata,
 )
 
 class Ils(object):
-	
 	def dic(self):
 		return {'ogc_fid': self.ogc_fid,
 				'navaid_id': self.navaid_id.strip(),
@@ -78,13 +76,13 @@ orm.mapper(Ils, t_ils)
 t_runway_threhold =  Table('runwaythreshold', metadata,
     Column(u'ogc_fid', INTEGER(), primary_key=True, nullable=False),
 	Column(u'wkb_geometry', String(length=255), primary_key=False),
-	Column(u'apt_icao', CHAR(length=4, convert_unicode=False, assert_unicode=None, unicode_error=None), primary_key=False),
+	Column(u'apt_icao', CHAR(length=4, convert_unicode=False, assert_unicode=None, unicode_error=None)),
 	Column(u'rwy_num', CHAR(length=3, convert_unicode=False, assert_unicode=None, unicode_error=None)),
 	Column(u'width_m', Float(precision=53, asdecimal=False), primary_key=False),
 	Column(u'surface', CHAR(length=11, convert_unicode=False, assert_unicode=None, unicode_error=None )),
 	Column(u'shoulder', CHAR(length=8, convert_unicode=False, assert_unicode=None, unicode_error=None )),
-	Column(u'smoothness', NUMERIC(precision=4, scale=2, asdecimal=True), primary_key=False),
-	Column(u'centerline_lights', NUMERIC(precision=1, scale=0, asdecimal=True), primary_key=False),
+	Column(u'smoothness', NUMERIC(precision=4, scale=2, asdecimal=True)),
+	Column(u'centerline_lights', NUMERIC(precision=1, scale=0, asdecimal=True)),
 	Column(u'edge_lighting', CHAR(length=4, convert_unicode=False, assert_unicode=None, unicode_error=None)),
 	Column(u'distance_remaining_signs', NUMERIC(precision=1, scale=0, asdecimal=True)),
 	Column(u'displaced_threshold_m', Float(precision=53, asdecimal=False)),
@@ -92,7 +90,7 @@ t_runway_threhold =  Table('runwaythreshold', metadata,
 	Column(u'stopway_length_m', Float(precision=53, asdecimal=False)),
 	Column(u'markings', CHAR(length=22, convert_unicode=False, assert_unicode=None, unicode_error=None, )),
 	Column(u'approach_lighting', CHAR(length=26, convert_unicode=False, assert_unicode=None, unicode_error=None, )),
-	Column(u'touchdown_lights', NUMERIC(precision=1, scale=0, asdecimal=True), primary_key=False),
+	Column(u'touchdown_lights', NUMERIC(precision=1, scale=0, asdecimal=True)),
 	Column(u'reil', CHAR(length=16, convert_unicode=False, assert_unicode=None, unicode_error=None, )),
 	Column(u'length_m', Float(precision=53, asdecimal=False)),
 	Column(u'true_heading_deg', NUMERIC(precision=6, scale=2, asdecimal=True)),    
@@ -109,3 +107,4 @@ class RunwayThreshold(object):
 				'true_heading_deg': str(self.true_heading_deg)
 		}
 orm.mapper(RunwayThreshold, t_runway_threhold)
+
